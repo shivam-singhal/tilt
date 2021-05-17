097aabe297b1bc446de66e0bbe4948b35abf58e9
Shivam Singhal <shivamsi@gmail.com>
2021-05-16 21:49:06 -0700

    done with part 1 code

--- tilt/ViewController.swift
+++ tilt/ViewController.swift
@@ -11,6 +11,7 @@ import CoreMotion
 class ViewController: UIViewController {
 
     @IBOutlet weak var accelBiasNoise: UIButton!
+    @IBOutlet weak var gyroBiasNoise: UIButton!
     @IBOutlet weak var aBias: UILabel!
     @IBOutlet weak var aNoise: UILabel!
     @IBOutlet weak var gBias: UILabel!
@@ -28,25 +29,33 @@ class ViewController: UIViewController {
         accelBiasNoise.isEnabled = false
     }
     
-    //    @IBAction func measureGyroBiasNoise(_ sender: Any) {
-//        //startGyros()
-//        gyroBiasNoise.isEnabled = false
-//    }
+    @IBAction func measureGyroBiasNoise(_ sender: Any) {
+        startGyros()
+        gyroBiasNoise.isEnabled = false
+    }
     
     @IBAction func stop(_ sender: Any) {
         accelBiasNoise.isEnabled = true
+        gyroBiasNoise.isEnabled = true
         stopAccels()
+        stopGyros()
     }
     
     let motion = CMMotionManager()
     var counter:Double = 0
+    let numSamples:Double = 200
     
     var timer_accel:Timer?
     var accelSum: [Double] = [Double](repeating: 0.0, count: 3)
+    var accelSum2: [Double] = [Double](repeating: 0.0, count: 3)
     var accelBias: [Double] = [Double](repeating: 0.0, count: 3)
     var accelNoise: [Double] = [Double](repeating: 0.0, count: 3)
     
-//    var timer_gyro:Timer?
+    var timer_gyro:Timer?
+    var gyroSum: [Double] = [Double](repeating: 0.0, count: 3)
+    var gyroSum2: [Double] = [Double](repeating: 0.0, count: 3)
+    var gyroBias: [Double] = [Double](repeating: 0.0, count: 3)
+    var gyroNoise: [Double] = [Double](repeating: 0.0, count: 3)
 //    var gyro_file_url:URL?
 //    var gyro_fileHandle:FileHandle?
     
@@ -75,21 +84,27 @@ class ViewController: UIViewController {
                 let text = "\(timestamp), \(x), \(y), \(z)\n"
                 print ("\(counter) A: \(text)")
                 
-                // TODO keep a running count
-                if counter < 200 {
+                if counter < numSamples {
                     counter = counter+1
                     accelSum[0] = accelSum[0] + x
                     accelSum[1] = accelSum[1] + y
                     accelSum[2] = accelSum[2] + z
+                    
+                    accelSum2[0] = accelSum2[0] + x*x
+                    accelSum2[1] = accelSum2[1] + y*y
+                    accelSum2[2] = accelSum2[2] + z*z
                 } else {
                     for i in 0...2 {
                         accelBias[i] = accelSum[i] / counter
+                        accelNoise[i] = accelSum2[i] / counter - accelBias[i] * accelBias[i]
                     }
                     print ("Accelerometer Bias: \(accelBias[0]), \(accelBias[1]), \(accelBias[2])")
+                    print ("Accelerometer Noise: \(accelNoise[0]), \(accelNoise[1]), \(accelNoise[2])")
                     
-                    aBias.text = "\(accelBias[0]), \(accelBias[1]), \(accelBias[2])"
-                    // TODO: reset bias/counter?
+//                    aBias.text = "\(accelBias[0]), \(accelBias[1]), \(accelBias[2])"
+
                     accelSum = [Double](repeating: 0.0, count: 3)
+                    accelSum2 = [Double](repeating: 0.0, count: 3)
                     counter = 0
                     stop((Any).self)
                 }
@@ -98,6 +113,56 @@ class ViewController: UIViewController {
 
           // Add the timer to the current run loop.
         RunLoop.current.add(self.timer_accel!, forMode: RunLoop.Mode.default)
+       }
+    }
+    
+    func startGyros() {
+       if motion.isGyroAvailable {
+          self.motion.gyroUpdateInterval = 1.0 / 60.0
+          self.motion.startGyroUpdates()
+                
+          // Configure a timer to fetch the accelerometer data.
+          self.timer_gyro = Timer(fire: Date(), interval: (1.0/60.0),
+                 repeats: true, block: { [self] (timer) in
+             // Get the gyro data.
+             if let data = self.motion.gyroData {
+                let x = data.rotationRate.x
+                let y = data.rotationRate.y
+                let z = data.rotationRate.z
+                                
+                let timestamp = NSDate().timeIntervalSince1970
+                let text = "\(timestamp), \(x), \(y), \(z)\n"
+                print ("\(counter) G: \(text)")
+                
+                if counter < numSamples {
+                    counter = counter+1
+                    gyroSum[0] = gyroSum[0] + x
+                    gyroSum[1] = gyroSum[1] + y
+                    gyroSum[2] = gyroSum[2] + z
+                    
+                    gyroSum2[0] = gyroSum2[0] + x*x
+                    gyroSum2[1] = gyroSum2[1] + y*y
+                    gyroSum2[2] = gyroSum2[2] + z*z
+                } else {
+                    for i in 0...2 {
+                        gyroBias[i] = gyroSum[i] / counter
+                        gyroNoise[i] = gyroSum2[i] / counter - gyroBias[i] * gyroBias[i]
+                    }
+                    print ("Gyro Bias: \(gyroBias[0]), \(gyroBias[1]), \(gyroBias[2])")
+                    print ("Gyro Noise: \(gyroNoise[0]), \(gyroNoise[1]), \(gyroNoise[2])")
+                    
+//                    aBias.text = "\(accelBias[0]), \(accelBias[1]), \(accelBias[2])"
+
+                    gyroSum = [Double](repeating: 0.0, count: 3)
+                    gyroSum2 = [Double](repeating: 0.0, count: 3)
+                    counter = 0
+                    stop((Any).self)
+                }
+             }
+          })
+
+          // Add the timer to the current run loop.
+          RunLoop.current.add(self.timer_gyro!, forMode: RunLoop.Mode.default)
        }
     }
     
@@ -110,16 +175,16 @@ class ViewController: UIViewController {
        }
     }
     
-//    func stopGyros() {
-//       if self.timer_gyro != nil {
-//          self.timer_gyro?.invalidate()
-//          self.timer_gyro = nil
-//
-//          self.motion.stopGyroUpdates()
-//
-////           gyro_fileHandle!.closeFile()
-//       }
-//    }
+    func stopGyros() {
+       if self.timer_gyro != nil {
+          self.timer_gyro?.invalidate()
+          self.timer_gyro = nil
+
+          self.motion.stopGyroUpdates()
+
+//           gyro_fileHandle!.closeFile()
+       }
+    }
     
 }
 
